Fix CLAP computation

ohc/fitness/clap.py
@@ -1,31 +1,38 @@
 from typing import List
 
+import numpy as np
 import torch
-from transformers import ClapModel, ClapProcessor
+from transformers import ClapModel, AutoTokenizer, AutoFeatureExtractor
 
 
 class CLAPSimilarity:
     model: ClapModel
-    processor: ClapProcessor
+    tokenizer: AutoTokenizer
+    feature_extractor: AutoFeatureExtractor
 
     def __init__(
         self,
         model_name="laion/larger_clap_general",
         device: str = "cuda" if torch.cuda.is_available() else "cpu",
+        sample_rate: int = 48000,
     ):
         self.device = device
         self._init_model(model_name, device)
+        self.sample_rate = sample_rate
 
     def _init_model(self, model_name: str, device: str):
         print(f"Initializing CLAP model: {model_name}")
         self.model = ClapModel.from_pretrained(model_name).to(device)
 
-        print(f"Initializing CLAP processor: {model_name}")
-        self.processor = ClapProcessor.from_pretrained(model_name).to(device)
+        print(f"Initializing CLAP tokenizer: {model_name}")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        print(f"Initializing CLAP feature extractor: {model_name}")
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
 
     def get_text_embedding(self, texts: List[str]) -> torch.Tensor:
-        features = self.processor(
-            text=texts,
+        features = self.tokenizer(
+            texts,
             padding=True,
             truncation=True,
             return_tensors="pt",
@@ -33,33 +40,29 @@ class CLAPSimilarity:
 
         return self.model.get_text_features(**features)
 
-    def get_audio_embedding(self, audio: torch.Tensor) -> torch.Tensor:
-        if audio.ndim == 1:
-            audio = audio[None, None]
-        elif audio.ndim == 2:
-            audio = audio[None]
-        elif audio.ndim != 3:
-            raise ValueError(f"Invalid audio shape: {audio.shape}")
+    def _preprocess_audio(self, audio: np.ndarray) -> np.ndarray:
+        if audio.ndim > 1:
+            audio = [audio[i] for i in range(audio.shape[0])]
+        else:
+            audio = [audio]
 
-        if audio.device != self.device:
-            audio = audio.to(self.device)
-
-        features = self.processor(
-            audios=audio,
-            padding=True,
-            truncation=True,
+        return self.feature_extractor(
+            audio,
             return_tensors="pt",
+            sampling_rate=self.sample_rate,
         )
 
-        return self.model.get_audio_features(**features)
+    def get_audio_embedding(self, audio: np.ndarray) -> torch.Tensor:
+        audio_features = self._preprocess_audio(audio)
+        return self.model.get_audio_features(**audio_features)
 
     def compute_similarity(
-        self, audio: torch.Tensor, target_embeddings: torch.Tensor
+        self, audio: np.ndarray, target_embeddings: torch.Tensor
     ) -> torch.Tensor:
         audio_embeddings = self.get_audio_embedding(audio)
         if target_embeddings.device != audio_embeddings.device:
             target_embeddings = target_embeddings.to(audio_embeddings.device)
-        return cosine_similarity_matrix(target_embeddings, audio_embeddings)
+        return cosine_similarity_matrix(audio_embeddings, target_embeddings)
 
 
 def cosine_similarity_matrix(a, b):
@@ -69,27 +72,23 @@ def cosine_similarity_matrix(a, b):
 
 
 def test_clap():
-    processor = ClapProcessor.from_pretrained("laion/larger_clap_general")
-    model = ClapModel.from_pretrained("laion/larger_clap_general")
+    clap = CLAPSimilarity()
 
-    inputs = processor(
-        text=[
+    targets = clap.get_text_embedding(
+        [
             "King",
-            "Prince",
             "Octopus",
-            "Suffering",
-            "Famine",
+            "There is no spoon",
+            "The cake is a lie",
             "Now this is a story all about how my life got flipped turned upside down",
         ],
-        padding=True,
-        truncation=True,
-        return_tensors="pt",
     )
-    print(inputs)
-    outputs = model.get_text_features(**inputs)
 
-    print(outputs)  # torch.Size([3, 2])
-    print(cosine_similarity_matrix(outputs, outputs))  # torch.Size([3, 3])
+    fake_audios = np.random.normal(0, 1, (10, 100))
+    similarity = clap.compute_similarity(fake_audios, targets)
+
+    print(similarity.shape)  # (5, 10)
+    print(similarity)
 
 
 if __name__ == "__main__":